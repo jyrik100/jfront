e51238d13a04ac756a85d49f3f41c70fd01a157b
jyk <jkartila@hotmail.com>
2023-01-30 10:27:49 +0200

    Add more data to single Journey entry

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -1,7 +1,25 @@
 const Journey = ({ journey }) => {  
     return (    
     <li>
-    id: {journey.id} departure: {journey.Departure}  
+    <div>
+      id: {journey.id}
+    </div>
+      departureTime: {journey.Departure}
+    <div>
+      departureFrom: {journey.Departure_station_name}
+    </div>
+    <div>
+     returnTime: {journey.Return}     
+    </div>
+    <div>
+      returnTo: {journey.Return_station_name}      
+    </div>
+    <div>
+      covered distance: {journey.Covered_distance_m/1000} km     
+    </div>
+    <div>
+      covered durance: {journey.Duration_sec/60} min     
+    </div>
     </li>  
     )
   }
