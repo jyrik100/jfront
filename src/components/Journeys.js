5760ffe88e04fdf69524ec2e638452916f37f80f
jyk <jkartila@hotmail.com>
2023-02-03 11:27:31 +0200

    fixes: 2decimals, skip too short entries

--- src/components/Journeys.js
+++ src/components/Journeys.js
@@ -10,9 +10,7 @@ const Journeys = () => {
   const [size, setPageSize] = useState(10);
   
   const handlePage = (page) => setPage(page);
-  const handlePageSizeChange = (event) => {
-    setPageSize(event.target.value);
-  }
+  const handlePageSizeChange = (event) => {setPageSize(event.target.value);}
   const totalPages = 30000 // Todo: this value should be dynamic
 //  const pageContent = journeys.slice((page - 1) * size, page * size)
 
@@ -39,6 +37,10 @@ const Journeys = () => {
   }
   const handleJourneyChange = (event) => {    
     setNewJourney(event.target.value)  
+  }
+
+  const decimals = (value) => {    // fix long doubles to show 2 decimals
+    return parseFloat(value.toFixed(2));
   }
     return (    
     <div>
@@ -82,8 +84,8 @@ const Journeys = () => {
                <TableCell>{journey.Departure}</TableCell>
                <TableCell>{journey.Return_station_name}</TableCell>
                <TableCell>{journey.Return}</TableCell>
-               <TableCell >{journey.Covered_distance_m/1000} km</TableCell>
-               <TableCell>{journey.Duration_sec/60} min</TableCell>
+               <TableCell >{decimals(journey.Covered_distance_m/1000)} km</TableCell>
+               <TableCell>{decimals(journey.Duration_sec/60)} min</TableCell>
              </TableRow>
            ))}
          </TableBody>
--- src/services/Journeys.js
+++ src/services/Journeys.js
@@ -1,17 +1,10 @@
 import axios from 'axios'
 const baseUrl1 = 'http://localhost:3001/api/journeys'
-//const baseUrl2 = 'http://localhost:3001/api/somejourneys/1/5'  // current page 4, pagesize 5
-
-const baseUrl2 = 'http://localhost:3001/api/pagejourneys'  // current page 4, pagesize 5
+const baseUrl2 = 'http://localhost:3001/api/pagejourneys'  // pass the page and size 
 
 const getAllJourneys = () => {
   return axios.get(baseUrl1)
 }
-
-/* const getSomeJourneys = () => {
-  return axios.get(baseUrl2)
-}
- */
 
 const getpageJourneys = (ppage, psize) => {
   return axios.get(`${baseUrl2}/${ppage}/${psize}`)
